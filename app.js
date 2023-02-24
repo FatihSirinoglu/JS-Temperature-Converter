2ab066f91d9c0ea3178cda8c4410a737c25ce8ad
sSsiriusSs <m.fatih.abd@gmail.com>
2023-02-24 09:35:50 +0300

    nice simple temperature converter app

---
+++ app.js
@@ -0,0 +1,29 @@
+const celciusEl = document.getElementById("celcius")
+const fahrenheitEl = document.getElementById("fahrenheit")
+const kelvinEl = document.getElementById("kelvin")
+
+
+
+
+function computeTemp(event){
+    // console.log(event.target.value)
+    // console.log(event.target.name)
+    const currentValue = +event.target.value
+
+    switch (event.target.name) {
+        case "celcius":
+            kelvinEl.value = (currentValue + 273.32).toFixed(2)
+            fahrenheitEl.value = (currentValue *1.8 + 32 ).toFixed(2)
+            break;
+        case "fahrenheit":
+            celciusEl.value = ((currentValue-32)/ 1.8).toFixed(2)
+            kelvinEl.value = ((currentValue-32)/1.8 +273.32).toFixed(2)
+            break;
+        case "kelvin":
+            celciusEl.value = (currentValue-273.32).toFixed(2)
+            fahrenheitEl.value = ((currentValue-273.32)*1.8 + 32).toFixed(2)  
+            break;
+        default:
+            break;
+    }
+}
